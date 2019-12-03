776594988661dc8e948442899504ad11d6001b54
Lydia Garcia <lydiagarcia.chan@gmail.com>
2019-12-03 10:28:40 -0300

    Organização de includes e classes

---
+++ classes/connection.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class Connection{
+
+	CONST HOST = '127.0.0.1';
+	CONST DB = 'andreiastore';
+	CONST USER = 'root';
+	CONST PASS = 'root';
+
+	public $con;
+
+}
+
+?>
--- sobre.php
+++ sobre.php
@@ -10,50 +10,7 @@
 </head>
 <body>
 
-	<header>
-		<div class="container-fluid mb-3">
-			<div class="container">
-				<div class="row">
-					<div class="col-12 mt-4">
-
-						<div class="row">
-							<div class="col-4">
-								<a href="index.php"><img src="media/img/banner.png" height="100"></a>
-							</div>
-
-							<div class="col-6 my-auto">
-								<div class="input-group">
-									<input type="text" class="form-control" aria-label="Recipient's username" placeholder="Pesquise..." aria-describedby="basic-addon2">
-									<button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
-								</div>
-							</div>
-
-							<div class="col-2 my-auto text-right">
-								<a href="#login"><i class="fas fa-user text-white"></i></a>
-							</div>
-						</div>
-
-					</div>
-				</div>
-			</div>
-		</div>
-		
-		<ul id="menu">
-			<div class="container-fluid">
-				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<a href="calcados.php"><li>Calçados</li></a>
-							<a href="semijoias.php"><li>Semijóias</li></a>
-							<a href="sobre.html"><li>Sobre</li></a>
-							<a href="index.php#contato"><li>Contato</li></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</ul>
-
-	</header>
+	<?php include_once "include/navbar.php"; ?>
 
 	<content>
 		
